1c7ba57996d691c2d66a02ed036441ae85865c46
方煜 <fangyu_wh@163.com>
2018-06-03 22:32:25 +0800

    scrapy startproject Article

---
+++ ArticleSpider/items.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# 定义我们所要爬取的信息的相关属性。Item对象是种类似于表单，用来保存获取到的数据
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ArticlespiderItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
---
+++ ArticleSpider/pipelines.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# 做跟数据存储相关的东西
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+class ArticlespiderPipeline(object):
+    def process_item(self, item, spider):
+        return item
